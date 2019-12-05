b9bcdcfefd9fe2985106782f735c6cc2e55e8c0e
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2019-12-05 09:04:23 -0500

    removed unused main file

--- ds/Stack/stack.py
+++ ds/Stack/stack.py
@@ -1,13 +1,19 @@
 class Stack:
 
-    def __init__(self) -> object:
-        self.__stack = [None] * 1000
+    def __init__(self, size=1000) -> object:
+        self.size = size
+        self.__stack = [None] * self.size
         self.__top = -1
+
+    def isEmpty(self):
+        if self.__top == 0:
+            return True
+        else:
+            return False
 
     def push(self, value):
         print("Push ", value)
-
-        if self.__top == 1000:
+        if self.__top == self.size:
             self.__top = 0
         else:
             self.__top = self.__top + 1
@@ -15,11 +21,15 @@ class Stack:
         self.__stack[self.__top] = value
 
     def pop(self):
-        temp = self.__stack[self.__top]
-        self.__stack[self.__top] = None
-        self.__top = self.__top - 1
-        print("Pop ", temp)
-        return temp
+        if self.isEmpty():
+            print("Stack is Empty.")
+            return None
+        else:
+            popped_element = self.__stack[self.__top]
+            self.__stack[self.__top] = None
+            self.__top = self.__top - 1
+            print("Pop ", popped_element)
+            return popped_element
 
     def print(self):
         stack_items = ""
@@ -27,3 +37,17 @@ class Stack:
             if item is not None:
                 stack_items += str(item) + " "
         print(stack_items)
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(3)
+    stack.push(7)
+    stack.push(2)
+    stack.push(1)
+    stack.print()
+    print("Popped: ", stack.pop())
+    stack.print()
+    stack.push(9)
+    stack.push(5)
+    stack.print()
