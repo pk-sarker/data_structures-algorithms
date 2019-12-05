e78da9fade519a4db4219cfc456e81eb605e79b9
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2019-12-04 21:12:47 -0500

    Updates stack code with empty stack check.

--- ds/Stack/stack.py
+++ ds/Stack/stack.py
@@ -4,9 +4,14 @@ class Stack:
         self.__stack = [None] * 1000
         self.__top = -1
 
+    def isEmpty(self):
+        if self.__top == 0:
+            return True
+        else:
+            return False
+
     def push(self, value):
         print("Push ", value)
-
         if self.__top == 1000:
             self.__top = 0
         else:
@@ -15,11 +20,15 @@ class Stack:
         self.__stack[self.__top] = value
 
     def pop(self):
-        temp = self.__stack[self.__top]
-        self.__stack[self.__top] = None
-        self.__top = self.__top - 1
-        print("Pop ", temp)
-        return temp
+        if self.isEmpty():
+            print("Stack is Empty.")
+            return None
+        else:
+            popped_element = self.__stack[self.__top]
+            self.__stack[self.__top] = None
+            self.__top = self.__top - 1
+            print("Pop ", popped_element)
+            return popped_element
 
     def print(self):
         stack_items = ""
