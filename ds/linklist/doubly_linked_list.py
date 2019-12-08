043f972b5dd576a5f123f2f05503933d74bfbf77
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2019-12-08 02:38:07 -0500

    Removed garbage codes

--- ds/linklist/doubly_linked_list.py
+++ ds/linklist/doubly_linked_list.py
@@ -76,68 +76,6 @@ class DoublyLinkedList:
             pointer = pointer.next
         return False
 
-    def swap1(self, node_a, node_b):
-        print("\nSwap node with value {} with node with value {}".format(node_a, node_b))
-        pointer = self.__head
-        pointer_a = None
-        pointer_b = None
-
-        while pointer is not None:
-            if pointer.data == node_a:
-                pointer_a = pointer
-            if pointer.data == node_b:
-                pointer_b = pointer
-            if pointer_a is not None and pointer_b is not None:
-                print("both: {} {}".format(pointer_a.data, pointer_b.data))
-
-                temp_a = pointer_a
-                temp_b = pointer_b
-
-                # a.prevNode.next -> a 1
-                # a.prev -> b.prevNode 3
-                # a.next -> b.nextNone 2
-                # a.nextNode.prev -> b 4
-
-                # b.prevNode.next -> a 5
-                # b.prev -> a.prev 6
-                # b.next -> a.next 7
-                # b.nextNode.prev -> a
-
-                # pointer_a.previous.next = pointer_b # 1 -
-                #
-                # pointer_a.next = pointer_b.next # 2 -
-                #
-                # pointer_a.previous = pointer_b.previous # 3 -
-                #
-                # temp_a.next.previous = pointer_b  # 4
-                #
-                # self.print_list()
-                # print(">> ", temp_b.next.data)
-                # print(">> ", temp_b.previous.data)
-
-                pointer_b.previous.next = pointer_a
-                # temp_b.next.previous = temp_a # 5
-                # #
-                pointer_b.next = pointer_a.next
-                # temp_b.previous = temp_a.previous # 6
-                # #
-                pointer_b.previous = pointer_a.previous
-
-                temp_b.next.previous = pointer_a
-                # temp_b.previous.next = temp_a.next # 7
-
-                # temp_a.next.previous = pointer_b
-                # pointer_b.previous = temp_a.next
-                # temp_b.previous = temp_a.previous # 8
-                # self.print_list()
-
-                self.print_list()
-                print(">> ", temp_b.next.data)
-                print(">> ", temp_b.previous.data)
-                break
-
-            pointer = pointer.next
-
     def swap(self, node_a, node_b):
         print("\nSwap node with value {} with node with value {}".format(node_a, node_b))
         pointer = self.__head
